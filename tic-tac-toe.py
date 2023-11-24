2dc182a2f2a9fb55628cac901806418a17d86db5
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-11-25 00:23:25 +0100

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,27 +3,29 @@ import random
 
 class Board:
     def __init__(self):
-        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.field = [[" " for _ in range(3)] for _ in range(3)]
 
     def display(self):
         print("-------")
-        for row in self.board:
+        for row in self.field:
             print("|" + "|".join(row) + "|")
             print("-------")
 
 
-class ComputerOlayer:
+class ComputerPlayer:
     def __init__(self, symbol):
         self.symbol = symbol
 
     def choose_move(self, board):
         self.board = board
-        self.move_complete = False
-        while not self.move_complete:
-            slot = self.board[random.randint(0, 2)][random.randint(0, 2)]
-            if slot == " ":
-                slot = self.symbol
-                self.move_complete = True
+        move_complete = False
+        while not move_complete:
+            column = random.randint(0, 2)
+            line = random.randint(0, 2)
+            if self.board.field[line][column] == " ":
+                self.board.field[line][column] = self.symbol
+                move_complete = True
+            return self.board
 
 
 class HumanPlayer:
@@ -39,30 +41,9 @@ class EndGame:
         pass
 
 
-def print_board(board):
-    """
-    Функція для виведення ігрового поля.
-    Матриця 3x3, де кожен елемент - це строка, що представляє клітинку поля:
-    " " - пуста клітинка, "X" - хрестик, "O" - нулик.
-    """
-    print("-------")
-    for row in board:
-        print("|" + "|".join(row) + "|")
-        print("-------")
+board = Board()
+board.display()
 
-
-# Створення пустого поля
-board = [[" " for _ in range(3)] for _ in range(3)]
-
-# Виведення пустого
-print_board(board)
-
-symbol = 'X'
-move_complete = False
-while not move_complete:
-    column = random.randint(0, 2)
-    line = random.randint(0, 2)
-    if board[line][column] == " ":
-        board[line][column] = symbol
-        move_complete = True
-    print_board(board)
+computer = ComputerPlayer('X')
+for _ in range(3):
+    computer.choose_move(board).display()
