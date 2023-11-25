38bd02c1c0f0329aebccf136e1a185735af216e7
Yan-Bakhmat <yan.bakhmat@gmail.com>
2023-11-25 01:53:51 +0100

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -28,25 +28,45 @@ class ComputerPlayer:
             if self.board.field[line][column] == " ":
                 self.board.field[line][column] = self.symbol
                 move_complete = True
-            return self.board
+        return self.board
 
 
 class HumanPlayer:
     def __init__(self, symbol):
         self.symbol = symbol
 
-    def make_move(self):
-        pass
+    def make_move(self, board):
+        self.board = board
+        decoder = {
+            'a': 1,
+            'b': 2,
+            'c': 3
+        }
+        tester_column = ['a', 'b', 'c']
+        tester_lines = ['1', '2', '3']
+        move_complate = False
+        while not move_complate:
+            choose = input("Choose a slot: ")
+            if len(choose) == 2 and choose[0] in tester_column and choose[1] in tester_lines and self.board.field[int(choose[1])][decoder[choose[0]]] == ' ':
+                self.board.field[int(choose[1])
+                                 ][decoder[choose[0]]] = self.symbol
+                move_complate = True
+            else:
+                print('Please select an existing slot')
+        return self.board
 
 
 class EndGame:
-    def __init__(self):
+    def __init__(self, board):
         pass
 
 
 board = Board()
 board.display()
 
-computer = ComputerPlayer('X')
+computer = ComputerPlayer('O')
 for _ in range(3):
     computer.choose_move(board).display()
+
+human = HumanPlayer('X')
+human.make_move(board).display()
